f683b5cadeee916d7602bc337b2ce0a7fc077a4c
OfentseXI <dofentse69@gmail.com>
2024-04-19 14:17:31 +0200

    8th debug: await added

--- escapeVanillaJS.js
+++ escapeVanillaJS.js
@@ -124,10 +124,10 @@ function findIntersection(setA, setB) {
 
 async function navigateLabyrinth(directions) {
     for (let direction of directions) {
-        // 🪲 Bug: No delay
-        new Promise(resolve => setTimeout(resolve, 1000));
-        console.log(`Navigating: ${direction.step}`);
+      // 🪲 Bug: No delay
+      await new Promise((resolve) => setTimeout(resolve, 0));
+      console.log(`Navigating: ${direction.step}`);
     }
     return "Congratulations! You've mastered the essentials of Vanilla JavaScript. Welcome to the world of React, where you'll build powerful and dynamic web applications. Let's dive in!";
-}
+  }
 
